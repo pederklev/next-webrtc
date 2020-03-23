55615a1c4101d4c3480cf1703e1f9f08b4ea25ea
Peder <pederklev@gmail.com>
2020-03-23 12:43:31 +0100

    start transition to typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,21 @@
-import { Link, Router } from "../routes";
-
-import { isClient, seedGen } from "../libs/utils";
+// import { Router } from "../routes";
+import Router from "next/router";
+import React from "react";
+import { seedGen } from "../libs/utils";
 
 export default class extends React.Component {
-  setChannel = (address, transactions, deposit) => {
+  setChannel = () => {
     const channelID = seedGen(10);
     console.log("Starting channel with ID: ", channelID);
     setTimeout(() => {
-      Router.pushRoute(`/channel/${channelID}`);
+      Router.push(`/channel/${channelID}`);
     }, 500);
   };
 
   render() {
     return (
       <div>
-        {/* {console.log(this.state.form)} */}
         <div>
-          {/* <Logo src={"/static/logo.svg"} /> */}
           <button onClick={this.setChannel}>Go to a channel</button>
         </div>
       </div>
