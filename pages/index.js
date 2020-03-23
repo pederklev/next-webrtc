568ea9d466489214b1e77a8d8f05ab284e1c40a8
Peder <pederklev@gmail.com>
2020-03-23 12:25:32 +0100

    delete some more stuff

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,9 @@
 import { Link, Router } from "../routes";
 
 import { isClient, seedGen } from "../libs/utils";
-import Setup from "../components/Setup";
 
 export default class extends React.Component {
-  state = {
-    visible: 0
-  };
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ visible: 1 });
-    }, 300);
-  }
-
   setChannel = (address, transactions, deposit) => {
-    this.setState({ visible: 0 });
     const channelID = seedGen(10);
     console.log("Starting channel with ID: ", channelID);
     setTimeout(() => {
@@ -24,13 +12,12 @@ export default class extends React.Component {
   };
 
   render() {
-    var { visible } = this.state;
     return (
       <div>
         {/* {console.log(this.state.form)} */}
         <div>
           {/* <Logo src={"/static/logo.svg"} /> */}
-          <Setup setChannel={this.setChannel} {...this.state} />
+          <button onClick={this.setChannel}>Go to a channel</button>
         </div>
       </div>
     );
